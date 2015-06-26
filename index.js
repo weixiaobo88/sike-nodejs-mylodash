9b6cacda89976c9005f6b312b885d5f4a6126c53
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-06-26 16:56:53 +0800

    zy: finish code to ensure a func called only once

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ var _ = {
 
 };
 
+_.once = function(fn) {
+  var invoked = false;
+  var result;
+
+  return function() {
+    if(invoked) {
+      return result;
+    } else {
+      result = fn();
+      invoked = true;
+      return result;
+    }
+  }
+ }
+
 module.exports = _;
