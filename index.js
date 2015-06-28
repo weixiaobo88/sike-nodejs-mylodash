af45db2abce67a0efb8ccbe97c1a5764da560302
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-06-28 09:33:12 +0800

    zy: memoize arrays with same first element

--- index.js
+++ index.js
@@ -15,6 +15,25 @@ _.once = function(fn) {
       return result;
     }
   }
- }
+}
+
+_.memoize = function(fn, cb) {
+  var result = {};
+
+  return function(cache_key) {
+    if(cache_key.length) {
+      new_key = cb(cache_key);
+      if (!result.hasOwnProperty(new_key)) {
+        result[new_key] = fn(cache_key);
+      }
+      return result[new_key];
+    } else {
+      if (!result.hasOwnProperty(cache_key)) {
+        result[cache_key] = fn(cache_key);
+      }
+      return result[cache_key];
+    }
+  };
+}
 
 module.exports = _;
